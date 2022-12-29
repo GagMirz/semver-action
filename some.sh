8f9d69b1b783157d6111ec15721243bc7518632a
GagMirz <gmirzoyan12@gmail.com>
2022-12-29 19:09:03 +0400

    fix

--- some.sh
+++
@@ -1,7 +0,0 @@
-
-for index in "${!tickets_array[@]}"; do
-    blocks="${blocks},${NEWLINE}
-                    {\"type\": \"mrkdwn\", /BNPL-2641
-                      \"text\": \"*Ticket${index}:* <https://mondu.atlassian.net/browse/${tickets_array[index]}| ${tickets_array[index]}>\"
-                    }"
-done
